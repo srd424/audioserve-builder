55a01e74acce3a5b403738c3e125d94c5dc6e24d
Steve Dodd <steved424@gmail.com>
2022-06-12 18:13:40 +0100

    Handle cargo profiles properly Stop npm scribbling in source directory

--- README.txt
+++ README.txt
@@ -1,3 +1,4 @@
-podman run -v $PWD/src:/src:ro -v $PWD/build-cache:/build -v $PWD/result:/result audioserve-builder
+podman run -e CARGO_RELEASE=<profile> -v <src-dir>:/src:ro -v <build-cache-dir>:/build -v <result-dir>/result:/result audioserve-builder
 
-build-cache is optional, will speed up repeated builds if you pass it in.
+CARGO_RELEASE defaults to 'release', pass 'dev' or 'debug' for a normal (and faster) development build.
+<build-cache-dir> is optional, will speed up repeated builds if you pass it in.
--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 set -x
 
 [ -z "$CARGO_RELEASE" ] && CARGO_RELEASE=release
+[ "$CARGO_RELEASE" = "debug" ] && CARGO_RELEASE=dev
 
 cd /src
 
@@ -14,11 +15,11 @@ export npm_config_cache=/build/npm
 
 mkdir -p $CARGO_TARGET_DIR $CARGO_HOME $npm_config_cache
 cp /cargo-config /$CARGO_HOME/config
-cargo build --target=aarch64-unknown-linux-gnu --${CARGO_RELEASE}
+cargo build --target=aarch64-unknown-linux-gnu --profile ${CARGO_RELEASE}
 cp $CARGO_TARGET_DIR/aarch64-unknown-linux-gnu/${CARGO_RELEASE}/audioserve /result
 
 cd client
-npm install
+npm --no-save install
 npm run build
 mkdir -p /result/client
 cp -av dist /result/client
